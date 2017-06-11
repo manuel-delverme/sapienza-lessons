c9742964f4033c18a78bd758a5c0c0a8b8615aef
Manuel <manuel.delverme@gmail.com>
2017-06-11 21:56:38 +0200

    [NLP] HW3 MVP

--- s1/nlp/hw3/src/homework3.py
+++ s1/nlp/hw3/src/homework3.py
@@ -1,71 +1,102 @@
-from nltk import Tree
-import urllib
-import networkx as nx
-from GoogleScraper import scrape_with_config, GoogleSearchError
-from GoogleScraper.database import ScraperSearch, SERP, Link
-from collections import defaultdict
-import nltk
-import xml.etree.ElementTree as ET
-import gzip
 import glob
-from src.howdoesittastelike import nlp as en_nlp
+import gzip
+import urllib
+import xml.etree.ElementTree
+import xml.etree.ElementTree as ET
+from collections import defaultdict
 
-wiki_path = "/mnt/netbook/home/obafedyrider/babelfied-wikipediaXML/"
+import networkx
+from GoogleScraper import scrape_with_config, GoogleSearchError
+from SPARQLWrapper import SPARQLWrapper, JSON
+from nltk import Tree
+
+from src.howdoesittastelike import nlp as en_nlp, disk_cache
+
+# wiki_path = "/mnt/netbook/home/obafedyrider/babelfied-wikipediaXML/"
+wiki_path = "/media/awok/02bf03c5-0155-4f12-b5fb-623e6d27ab31/opt/babelfied-wikipediaXML/"
 
 archives_path = "{}/*".format(wiki_path)
+FOOD_OR_TOOL = """
+PREFIX schema: <http://schema.org/>
+PREFIX wikibase: <http://wikiba.se/ontology#>
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+SELECT ?x ?country ?article WHERE {
+{?x wdt:P31 wd:Q39546.}
+ UNION
+{?x wdt:P31 wd:Q2095.}
+?article schema:about ?x .
+?article schema:inLanguage "en" .
+?article schema:isPartOf <https://en.wikipedia.org/> .
+}
+LIMIT 200
+"""
 
-print(en_nlp("the cake is a lie"))
+ANIMALS_SPARQL = """
+PREFIX schema: <http://schema.org/>
+
+SELECT DISTINCT ?x ?article WHERE {
+  ?x wdt:P31 wd:Q16521.
+  ?x wdt:P105 wd:Q68947.
+  ?article schema:about ?x.
+  ?article schema:inLanguage "en".
+  ?article schema:isPartOf <https://en.wikipedia.org/>.
+}
+LIMIT 200
+"""
 
 
-def generate_interesting_archives(verbs):
-    pages = google_wikipedia_pages(verbs)
-    for verb in verbs:
-        archives = pages[verb]
-        for archive_name in archives:
-            normalized_name = archive_name.replace("_", "+")
-            archive_glob = "{}/{}.xml.gz".format(archives_path, normalized_name)
-            try:
-                try:
-                    archive_path = glob.glob(archive_glob)[0]
-                except IndexError as e:
-                    normalized_name = urllib.parse.quote_plus(archive_name.replace("_", " "))
-                    archive_glob = "{}/{}.xml.gz".format(archives_path, normalized_name)
-                    archive_path = glob.glob(archive_glob)[0]
-            except Exception as e:
-                print("ERROR", e)
-                raise e
-            yield archive_path
-
-
-def google_wikipedia_pages(keywords):
-    interesting_pages = defaultdict(list)
-    config = {
-        'SCRAPING': {
-            'scrape_method': 'http',
-            'use_own_ip': 'True',
-            # 'keyword': 'Let\'s go bubbles!',
-            'search_engines': 'google',
-            'num_pages_for_keyword': 1
-        },
-        # 'SELENIUM': {
-        #     'sel_browser': 'chrome',
-        # },
-        'GLOBAL': {
-            'do_caching': 'True'
-        }
-    }
-
-    for keyword in keywords:
-        config['keyword'] = '"{}" site:https://en.wikipedia.org/ -title:Talk'.format(keyword)
+def generate_interesting_archives(keyword):
+    pages = google_wikipedia_pages(keyword)
+    archives = pages[keyword]
+    for archive_name in archives:
         try:
-            sqlalchemy_session = scrape_with_config(config)
-        except GoogleSearchError as e:
-            print("ERROR:", e)
-        for search in sqlalchemy_session.query(ScraperSearch).all():
-            for serp in search.serps:
-                for link in serp.links:
-                    if link.link_type == 'results':
-                        interesting_pages[keyword].append(link.link.split("/")[-1])  # url parsing is hard
+            yield get_archive_path(archive_name)
+        except IndexError:
+            print("COULD NOT DECODE", archive_name, "PATH")
+
+
+@disk_cache
+def get_archive_path(archive_name):
+    normalized_name = archive_name.replace("_", "+")
+    archive_glob = "{}/{}.xml.gz".format(archives_path, normalized_name)
+    try:
+        archive_path = glob.glob(archive_glob)[0]
+    except IndexError:
+        try:
+            normalized_name = urllib.parse.quote_plus(archive_name.replace("_", " "))
+            archive_glob = "{}/{}.xml.gz".format(archives_path, normalized_name)
+            archive_path = glob.glob(archive_glob)[0]
+        except IndexError as e:
+            print("[ERROR] lookup failed", archive_name)
+            raise e
+    return archive_path
+
+
+@disk_cache
+def google_wikipedia_pages(keyword):
+    interesting_pages = defaultdict(set)
+    config = {
+        'scrape_method': 'http',
+        'use_own_ip': 'True',
+        'search_engines': 'google',
+        # 'search_engines': 'duckduckgo',
+        'num_pages_for_keyword': 10,
+        'do_caching': 'True',
+        'keyword': '"{}" site:https://en.wikipedia.org/ -title:Talk'.format(keyword),
+    }
+    try:
+        scraper_search = scrape_with_config(config)
+    except GoogleSearchError as e:
+        print("ERROR:", e)
+    else:
+        # for search in sqlalchemy_session.query(ScraperSearch).all():
+        for serp in scraper_search.serps:
+            for link in serp.links:
+                if link.link_type == 'results':
+                    page = link.link.split("/")[-1]
+                    page = page.split("&")[0]
+                    interesting_pages[keyword].add(page)  # url parsing is hard
     return dict(interesting_pages)
 
 
@@ -76,52 +107,43 @@ def to_nltk_tree(node):
         return node.orth_
 
 
-def main():
-    # global en_nlp
-    verbs = ["tastes like"]
-    for wikipedia_archive_path in generate_interesting_archives(verbs):
-        print("unzippping", wikipedia_archive_path)
-        with gzip.open(wikipedia_archive_path) as fin:
-            xml_page = ET.parse(fin)
-        print("parsing xml")
-        annotations = xml_page.findall("annotations")[0]
+def extract_paths(sent):
+    print_tree(sent)
+    graph = networkx.Graph()
+    root = sent.root
+    graph.add_node(root)
+    frontier = list(root.children)
+    for child in frontier:
+        graph.add_node(child)
+        graph.add_edge(root, child)
 
-        text_xml_node = xml_page.find('text')
-        doc = en_nlp(text_xml_node.text.split("\n")[0])
-        mergelist = {}
-        print_tree(doc)
-        len_sum = 0
-        indices = [0]
-        for idx, tok in enumerate(doc._py_tokens):
-            len_sum += len(tok) + 1
-            indices.append(len_sum)
-        # for np in parsed_tree.noun_chunks:
-        #     tok = np.merge(np.root.tag_, np.text, np.root.ent_type_)
-        #     print_tree(parsed_tree)
+    while frontier:
+        node = frontier.pop(0)
 
-        for annotation in annotations:
-            mention = annotation.find("mention").text
-            start = indices[int(annotation.find("anchorStart").text)]
-            end = indices[int(annotation.find("anchorEnd").text)]
-            bid = annotation.find("babelNetID").text
-            print("merging", doc.text[start:end])
-            tok = doc.merge(start, end, bid=bid, mention=mention)
-            print_tree(doc)
+        children = list(node.children)
+        frontier.extend(children)
 
-        # sents = nltk.sent_tokenize(node.text.lower())
-        for sent in doc.sents:
-            for verb in verbs:
-                if verb in sent.text.lower():
-                    print("FOUND", verb, "IN", sent)
-                    # print_tree(sent)
-                    # _ = input()
+        for child in children:
+            graph.add_node(child)
+            graph.add_edge(node, child)
+    paths = networkx.shortest_path(graph)
+
+    # Need to create a layout when doing
+    # separate calls to draw nodes and edges
+    # pos = networkx.spring_layout(graph)
+    # networkx.draw_networkx_nodes(graph, pos, cmap=plt.get_cmap('jet'))
+    # networkx.draw_networkx_edges(graph, pos, edgelist=graph.edges(), edge_color='r', arrows=True)
+
+    # for src in list(paths.keys()):
+    #     for dst in list(paths[src].keys()):
+    #         paths[src][dst] = paths[src][dst][1:-1]
+    #         if not paths[src][dst]:
+    #             del paths[src][dst]
+    return [paths[src][dst] for src in paths for dst in paths[src]]
 
 
-def print_tree(doc):
-    # for tok in doc:
-    #     print(tok.text)
-    _ = [to_nltk_tree(sent.root).pretty_print() for sent in doc.sents]
-    # build_tree(sent)
+def print_tree(root):
+    _ = [to_nltk_tree(root).pretty_print()]
 
 
 def build_tree(node):
@@ -131,14 +153,268 @@ def build_tree(node):
         return node.orth_
 
 
-# for np in doc.noun_chunks:
-#     print(
-#             "text,", np.text,
-#             "root.text", np.root.text,
-#             "root,dep_", np.root.dep_,
-#             "root,head.text", np.root.head.text)
-#     # I I nsubj like
-#     # green eggs eggs dobj like
-#     # ham ham conj eggs
+def fetch_wiki_xml(wikipedia_archive_path):
+    article_path = wikipedia_archive_path[:-3]
+    try:
+        with open(article_path) as fin:
+            xml_page = ET.parse(fin)
+            print("\t\t\treading", article_path)
+    except (FileNotFoundError, xml.etree.ElementTree.ParseError):
+        try:
+            with gzip.open(wikipedia_archive_path) as fin:
+                print("\t\t\tunzippping", wikipedia_archive_path)
+                content = fin.read()
+                xml_page = ET.ElementTree(ET.fromstring(content))
+                with open(article_path, "wb") as fout:
+                    fout.write(content)
+        except (FileNotFoundError, xml.etree.ElementTree.ParseError) as e:
+            print("SKIPPING", wikipedia_archive_path, e)
+            raise e
+            # with tarfile.open(
+            #         "/media/awok/02bf03c5-0155-4f12-b5fb-623e6d27ab31/opt/babelfied-wikipediaXML.tar.gz") as tar:
+            #     member = "/".join(article_path.split("/")[-3:])
+            #     tar.extract(member=member, path=wikipedia_archive_path)
+            # xml_page = fetch_wiki_xml(wikipedia_archive_path)
+    return xml_page
+
+
+# @disk_cache BROKEN!
+def extract_sents(wikipedia_archive_path):
+    if ".txt" == wikipedia_archive_path[-4:]:
+        with open(wikipedia_archive_path) as fin:
+            doc = en_nlp(fin.read())
+    else:
+        try:
+            xml_page = fetch_wiki_xml(wikipedia_archive_path)
+        except FileNotFoundError:
+            return []
+        annotations = xml_page.findall("annotations")[0]
+        text_xml_node = xml_page.find('text')
+        if text_xml_node.text is None:
+            return []
+        doc = en_nlp(text_xml_node.text)
+        for annotation in reversed(annotations):
+            mention = annotation.find("mention").text
+            start = int(annotation.find("anchorStart").text)
+            end = int(annotation.find("anchorEnd").text)
+            bid = annotation.find("babelNetID").text
+            span = doc[start:end]
+            span.merge(bid=bid, mention=mention)
+    return list(doc.sents)
+
+
+def fitler_paths(paths, patterns):
+    good_paths = []
+    triples = []
+    for path in paths:
+        for pattern_words, pattern_poss in patterns:
+            pattern_followed = False
+            word, pos = pattern_words[0], pattern_poss[0]
+            for path_pos, tok in enumerate(path):
+                if word == str(tok) and tok.pos_ == pos:
+                    pattern_start = path_pos
+                    word_found = 1
+                    for word, pos, tok in zip(pattern_words[1:], pattern_poss[1:], path[pattern_start + 1:]):
+                        if word != str(tok) or pos != tok.pos_:
+                            break
+                        else:
+                            word_found += 1
+                    if word_found == len(pattern_words):
+                        pattern_followed = True
+                        break
+            if pattern_followed and len(path) > len(pattern_words) + 1:
+                pre = path[:pattern_start]
+                pattern = path[pattern_start:pattern_start + len(pattern_words)]
+                post = path[pattern_start + len(pattern_words):]
+
+                pre_text = " ".join(map(str, pre))
+                pattern_text = " ".join(map(str, pattern))
+                post_text = " ".join(map(str, post))
+
+                if len(pre) == 1 and pre[0].pos_ == "NOUN" and len(post) == 1 and post[0].pos_ == "NOUN":
+                    triples.append((pre_text, pattern_text, post_text))
+                    good_paths.append(path)
+    return good_paths
+
+
+def is_same(tok, required_tok):
+    return (str(tok) == required_tok[0] or required_tok == "") and (
+        tok.pos_ == required_tok[1] or required_tok[1] == "")
+
+
+def extract_triples(root, pattern):
+    # print_tree(root)
+    passtrough = ["it", "that"]
+    if pattern['self'][0] in ("confused"):
+        passtrough.append("not")
+    lefts = root.lefts
+    if pattern['self'][0] in ("able", "similar"):
+        lefts = ["it"]
+    for left in (l for l in lefts if str(l).lower() in passtrough or l.pos_ in ("NOUN", "PROPN")):
+        # if left.pos_ == "NOUN":
+        # if pattern['lefts'] and is_same(left, pattern['lefts'][0]):
+        for right in root.rights:
+            if 'y' in pattern:
+                y_pos = pattern['y']
+            else:
+                y_pos = ("NOUN", "PROPN")
+            if pattern['rights'] and is_same(right, pattern['rights'][0]):
+                for rightright in right.children:
+                    if rightright.pos_ in y_pos:
+                        # other = " ".join(map(str, rightright.subtree))
+                        other = str(rightright)
+                        if str(left) == "not":
+                            left = "it"
+                        triple = [left, tuple([root] + [pattern['rights'][0][0]]), other]
+                        yield triple
+            else:
+                if right.pos_ in y_pos:
+                    triple = [left, tuple([root]), right]
+                    yield triple
+
+
+def gather_wiki_links(results):
+    paths = []
+    for result in results["results"]["bindings"]:
+        entity_id = result['article']["value"].split("/")[-1]
+        entity_id = urllib.parse.unquote(entity_id)
+        try:
+            paths.append(get_archive_path(entity_id))
+        except IndexError:
+            pass
+    return paths
+
+
+def gather_patterns():
+    patterns = {
+        'confused': {
+            'lefts': [],
+            # 'self': ("confused", "VERB"),
+            # 'rights': [("with", "")],
+            'self': ("similar", ""),
+            'rights': [("to", "")],
+            'pages': gather_wiki_links(sparqlery(ANIMALS_SPARQL)),
+        },
+        'tastes': {
+            'lefts': [],
+            'self': ("tastes", "VERB"),
+            'rights': [("like", "ADP")],
+            'pages': generate_interesting_archives("tastes like")
+        },
+        'part': {
+            'lefts': [],
+            'self': ("has", "VERB"),
+            'rights': [],
+            'pages': gather_wiki_links(sparqlery(FOOD_OR_TOOL))
+        },
+        'activity': {
+            'lefts': [],
+            'self': ("able", ""),
+            'rights': [],
+            'y': ["VERB"],
+            'pages': gather_wiki_links(sparqlery(ANIMALS_SPARQL))
+        }}
+    # Relation: activity
+    # Categories: Animals
+    # Description: describes the actions a living entity can perform
+    # Example Sentence: CONCEPT is able to...
+    # patterns['able'] = {
+    #     # 'lefts': [("is", "VERB"), ],
+    #     'category': "animals",
+    #     'self': ("able", ""),
+    #     'rights': [("to", ""), ],
+    # }
+    # Relation: similarity
+    # Categories: Animals, Instruments
+    # Description: describes similar concepts
+    # Example Sentence: CONCEPT could be confused with ...
+    # patterns['confused'] = {
+    #     'lefts': [],  # [("can", "ADP"), ("be", "VERB")],
+    #     'self': ("confused", "VERB"),
+    #     'rights': [("with", "with the"), ],
+    # }
+    # Relation: part
+    # Categories: Animals, Food, Vehicles, Clothes, Home, Appliance, Instruments, Artefacts, Tools, Container
+    # Description: describes meronymy relations
+    # Example Sentence: CONCEPT has...
+    # solve as wikidata sparql
+    # results = sparqlery("""
+    # SELECT ?x ?y ?xLabel ?yLabel WHERE {
+    #     ?x wdt:P527 ?y.
+    #     ?x wdt:P31 wd:Q39546.
+    #     SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+    # }
+    # LIMIT 20
+    # """, "has-part")
+    return patterns
+
+
+def walk_unzipped_wikipedia():
+    for i in range(1, 667):
+        archive_glob = "{}/{}.xml".format(archives_path.replace("*", str(i)), "*")
+        for path in glob.glob(archive_glob):
+            yield path + ".gz"
+
+
+def walk_wikipedia():
+    for i in range(1, 667):
+        archive_glob = "{}/{}.xml.gz".format(archives_path.replace("*", str(i)), "*")
+        for path in glob.glob(archive_glob):
+            yield path
+
+
+def defie(pattern_description, pages=walk_wikipedia()):
+    gathered_triples = set()
+    context = defaultdict(list)
+    for wikipedia_archive_path in pages:
+        sents = extract_sents(wikipedia_archive_path)
+        for sent in sents:
+            roots = [root for root in sent if list(root.children)]
+            for root in roots:
+                if str(root) == pattern_description['self'][0]:
+                    print_tree(root)
+                    triples = extract_triples(root, pattern_description)
+                    for triple in triples:
+                        for i, t in enumerate(triple):
+                            if str(t).lower() in ("that", "it"):
+                                page_name = wikipedia_archive_path.split("/")[-1].split(".")
+                                page_name = "".join(p for p in page_name if p not in ("tar", "gz", "xml"))
+                                triple[i] = page_name
+                        gathered_triples.add(tuple(triple))
+                        context[tuple(triple)].append(sent)
+    return gathered_triples, context
+
+
+@disk_cache
+def sparqlery(query):
+    sparql = SPARQLWrapper('https://query.wikidata.org/sparql', returnFormat=JSON)
+    sparql.setQuery(query)
+    results = sparql.query().convert()
+    return results
+
+
+def main():
+    triples = {}
+    patterns = gather_patterns()
+    for pattern_id, pattern_description in patterns.items():
+        try:
+            with open("results/" + pattern_id) as fin:
+                tuples = fin.read()
+            if not tuples or len(tuples.split("\n")) < 20:
+                raise FileNotFoundError()
+        except (FileNotFoundError,):
+            print("[PARSING]", pattern_id)
+            pages = pattern_description['pages']
+            gathered_triples, context = defie(pattern_description, pages=pages)
+            triples[pattern_id] = gathered_triples
+            with open("results/" + pattern_id, 'w') as fout:
+                fout.write("\n".join(map(str, gathered_triples)))
+            print(pattern_id, ":")
+            for trip in gathered_triples:
+                print(trip)
+                for c in context[trip]:
+                    print("\t\t", c)
+
+
 if __name__ == "__main__":
     main()
--- s1/nlp/hw3/src/howdoesittastelike.py
+++ s1/nlp/hw3/src/howdoesittastelike.py
@@ -1,5 +1,8 @@
 import pickle
+import hashlib
+import math
 import re
+import multiprocessing as mp
 from collections import defaultdict
 from typing import List, Dict, AnyStr
 
@@ -15,7 +18,8 @@ spacy_classifier = None
 def nlp(word):
     global spacy_classifier
     if not spacy_classifier:
-        spacy_classifier = spacy.load("en")
+        # spacy_classifier = spacy.load("en")
+        spacy_classifier = spacy.load("en_default")
     return spacy_classifier(word)
 
 
@@ -25,16 +29,33 @@ API_KEY = "8f8ddfde6c9fd54070517b1396c3f12a"
 
 
 def disk_cache(f):
-    cache_file = "cache/{}.pkl".format(f.__name__)
-
     def wrapper(*args, **kwargs):
+        fid = f.__name__
+        if "sparql" in f.__name__:
+            fid += hashlib.sha1("::".join(str(arg) for arg in args).encode('utf-8')).hexdigest()
+        elif args:
+            fid += "::".join(str(arg) for arg in args)
+        # if kwargs:
+        #     raise NotImplementedError("kwargs not supported")
+        cache_file = "cache/{}.pkl".format(fid)
         try:
             with open(cache_file, "rb") as fin:
                 retr = pickle.load(fin)
+            if f.__name__ == "extract_sents":
+                for idx, dump_name in list(enumerate(retr)):
+                    retr[idx] = spacy_classifier.from_file(dump_name)
         except FileNotFoundError:
             retr = f(*args, **kwargs)
+            if f.__name__ == "extract_sents":
+                for idx, sent in list(enumerate(retr)):
+                    sent_id = hashlib.sha1(sent.text.encode('utf-8')).hexdigest()
+                    dump_name = cache_file + sent_id
+                    sent.to_file(dump_name)
+                    retr[idx] = dump_name
             with open(cache_file, "wb") as fout:
                 pickle.dump(retr, fout)
+        except TypeError:
+            retr = spacy_classifier.from_disk(cache_file)
         return retr
 
     return wrapper
@@ -93,7 +114,7 @@ def get_recipe_ingredients(recipe_name):
                         if len(text) > 1:
                             if text[-1] == "s":
                                 text = text[:-1]
-                            if text not in badwords:
+                            if text not in badwords and text not in ingredients:
                                 ingredients.append(text)
             if ingredients:
                 ingredients_str = (" ".join(ingredients)).strip()
@@ -103,12 +124,13 @@ def get_recipe_ingredients(recipe_name):
     return recipe_ingredients
 
 
-def query_knowledge(food_to_dim, dish_name):
+def query_knowledge(food_to_dim, dish_name, food_frequency):
     x = np.zeros(shape=(1, len(food_to_dim)), dtype=np.float32)
     ingredients = get_recipe_ingredients(dish_name)
     for ingredient, weight in ingredients.items():
         idx = food_to_dim[ingredient]
-        x[0, idx] = weight
+        food_tastiness = food_frequency[idx]  # math.log(1 + food_frequency[idx])
+        x[0, idx] = weight / food_tastiness
     return x
 
 
@@ -118,6 +140,7 @@ def generate_lookup():
     ridx = 0
     food_to_dim = {}
     recipe_to_dim = {}
+    food_frequency = []
     global recipes_db
 
     for recipe_name in list(recipes_db.keys()):
@@ -134,24 +157,28 @@ def generate_lookup():
             for ingredient, quantity in recipe.items():
                 if ingredient not in food_to_dim:
                     food_to_dim[ingredient] = idx
+                    food_frequency.append(0)
                     idx += 1
-    return food_to_dim, recipe_to_dim
+                food_frequency[food_to_dim[ingredient]] += 1
+    return food_to_dim, recipe_to_dim, food_frequency
 
 
 def query_knowledge_thread(data):
     print("query: ", data[0])
     try:
-        return query_knowledge(data[1], data[0])
+        return query_knowledge(data[1], data[0], data[2])
     except RecipeNotFoundError:
         return np.array(False)
 
 
 @disk_cache
-def calculate_recipe_vectors(food_to_dim):
+def calculate_recipe_vectors(*args, food_to_dim=None, food_frequency=None):
+    if args:
+        raise DeprecationWarning("no args plx")
     global recipes_db
-    thread_data = ((food_name, food_to_dim) for food_name in list(recipes_db.keys()))
+    thread_data = ((food_name, food_to_dim, food_frequency) for food_name in list(recipes_db.keys()))
     # with mp.Pool() as pool:
-    # X = pool.map(query_knowledge_thread, thread_data)
+    #     X = pool.map(query_knowledge_thread, thread_data)
     X = map(query_knowledge_thread, thread_data)
     X = [xi for xi in X if xi.any()]
     X = np.vstack(X)
@@ -161,41 +188,50 @@ def calculate_recipe_vectors(food_to_dim):
 # 5706 normalizer
 def main():
     print("lookup")
-    food_to_dim, recipe_to_dim = generate_lookup()
+    food_to_dim, recipe_to_dim, food_frequency = generate_lookup()
     print("calc vectors")
-    X = calculate_recipe_vectors(food_to_dim)
+    X = calculate_recipe_vectors(food_to_dim=food_to_dim, food_frequency=food_frequency)
 
     print("build tree")
-    tree = NearestNeighbors(n_neighbors=5, algorithm='ball_tree', metric="euclidean").fit(X)
+    tree = NearestNeighbors(n_neighbors=5, algorithm='kd_tree', metric="l2").fit(X)
 
-    for food_name in list(recipes_db.keys())[:10]:
-        print_similar_foods(food_to_dim, recipe_to_dim, tree, food_name)
+    # for food_name in list(recipes_db.keys())[:10]:
+    #     print_similar_foods(food_to_dim, recipe_to_dim, food_frequency, tree, food_name)
 
-    print_similar_foods(food_to_dim, recipe_to_dim, tree, "pizza")
-    print_similar_foods(food_to_dim, recipe_to_dim, tree, "valentina")
+    print_similar_foods(food_to_dim, recipe_to_dim, food_frequency, tree, "pizza")
+    print_similar_foods(food_to_dim, recipe_to_dim, food_frequency, tree, "valentina")
 
 
-def print_similar_foods(food_to_dim, recipe_to_dim, model, food_name):
+def print_similar_foods(food_to_dim, recipe_to_dim, food_frequency, model, dish_name):
     try:
-        X_full = query_knowledge(food_to_dim, food_name)
+        X_full = query_knowledge(food_to_dim, dish_name, food_frequency)
     except RecipeNotFoundError:
-        print(food_name, "found no recipe")
+        print(dish_name, "found no recipe")
     else:
-        print("foods most similar to:", food_name, "recipe:")
-        ingredients = get_recipe_ingredients(food_name)
-        print([(name, int(weight * 100)) for name, weight in sorted(ingredients.items(), key=lambda x: -x[1]) if
-               weight > 0.01])
+        dim_to_food = {val: key for key, val in food_to_dim.items()}
+        print("dishes most similar to:", dish_name, "recipe:")
+
+        # ingredients = get_recipe_ingredients(dish_name)
+
+        def pprint_food(X):
+            X[0] /= X[0].sum()
+            for food_dim in reversed(X[0].argsort()):
+                food_name = dim_to_food[food_dim]
+                weight = X[0][food_dim] * 100
+                if weight > 1:
+                    print(food_name, int(weight * 10) / 10, end=', ', sep=" ")
+            print("")
+
+        pprint_food(query_knowledge(food_to_dim, dish_name, food_frequency))
         print("-" * 30)
 
         # indices = model.ann(X_full, 5, votes_required=4)
-        distances, indices = model.kneighbors(X_full)
+        distances, indices = model.kneighbors(X_full, n_neighbors=10)
         for distance, idx in zip(distances[0], indices[0]):
-            similar_recipe_name = [k for k in recipe_to_dim if recipe_to_dim[k] == idx][0]
-            ingredients = get_recipe_ingredients(similar_recipe_name)
-            print(similar_recipe_name, int(distance * 100),
-                  [(name, int(weight * 100)) for name, weight in sorted(ingredients.items(), key=lambda x: -x[1]) if
-                   weight > 0.01])
-
+            similar_dish_name = [k for k in recipe_to_dim if recipe_to_dim[k] == idx][0]
+            # ingredients = get_recipe_ingredients(similar_recipe_name)
+            print(similar_dish_name, distance, end=": ")
+            pprint_food(query_knowledge(food_to_dim, similar_dish_name, food_frequency))
 
 if __name__ == "__main__":
     recipes_db = sqliteshelve.open("cache/recipes.sqlite")
