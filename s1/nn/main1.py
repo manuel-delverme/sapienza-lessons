11289085c121c2fbe932d8e5c41dd5afcd0ee5f5
Manuel <manuel.delverme@gmail.com>
2018-08-12 22:05:06 +0100

    [NN] lazyimport and using chalearn datasets

--- s1/nn/main1.py
+++ s1/nn/main1.py
@@ -1,16 +1,17 @@
 # import GPy
-import zipfile
+import lazy_import
 import warnings
-
 warnings.filterwarnings("ignore")
 
-import GPyOpt
-import time
-import numpy as np
-import sklearn.metrics
-from sklearn.utils.extmath import density
-import models
-import sklearn.linear_model
+zipfile = lazy_import.lazy_module("zipfile")
+glob = lazy_import.lazy_module("glob")
+GPyOpt = lazy_import.lazy_module("GPyOpt")
+time = lazy_import.lazy_module("time")
+np = lazy_import.lazy_module("numpy")
+sklearn = lazy_import.lazy_module("sklearn")
+sklearn.metrics = lazy_import.lazy_module("sklearn.metrics")
+sklearn.model_selection = lazy_import.lazy_module("sklearn.model_selection")
+models = lazy_import.lazy_module("models")
 
 RANDOM_STATE = 31337
 
@@ -18,7 +19,7 @@ RANDOM_STATE = 31337
 # lets plot a matrix for paramters comparison and correlation checking:
 print("=" * 20, "optim", "=" * 20)
 print("\n" * 2)
-print("________________________")
+print(" ________________________")
 print("<# 1-binary-classification/>")
 print(" ------------------------")
 print("        \   ^__^")
@@ -31,63 +32,74 @@ print("=" * 20, "optim", "=" * 20)
 
 
 def load_datasets(challenge_nr):
-    import ipdb; ipdb.set_trace()
-    for dataset_zip in glob.glob("datasets/{}/*".format(challenge_nr)):
-        dataset_name = dataset_zip.split("/")[-1][:-3]
+    for dataset_zip in glob.glob("datasets/{}/*.zip".format(challenge_nr)):
+        dataset_name = dataset_zip.split("/")[-1][:-4]
         with zipfile.ZipFile(dataset_zip, "r") as f:
-            data = f.read(dataset_name + "_train.data")
-            targets = f.read(dataset_name + "_train.solution")
-    return sklearn.model_selection.train_test_split(data, target, random_state=31337)
+            print("\t", "loading", dataset_name)
+            train_file_name = dataset_name + "_train.data"
+            # data = f.read(dataset_name + "_train.data").decode('utf-8')
+            data = np.genfromtxt(train_file_name, delimiter=' ')
 
-for X_train, X_test, y_train, y_test in load_datasets(challenge_nr):
+            test_file_name = dataset_name + "_train.solution"
+            # target = (f.read(dataset_name + "_train.solution")).decode('utf-8')
+            # target = [int(d) for d in target.split("\n")[:-1]]
+            target = np.genfromtxt(test_file_name, delimiter=' ')
+            # print("dataset\n", f.read(dataset_name + "_public.info").decode('utf-8'))
 
-results = []
-losses = ('squared_hinge', 'hinge')
+        print("\t", len(data), datapoints)
+        assert(len(data) == len(target))
+        yield sklearn.model_selection.train_test_split(data, target, random_state=31337)
 
-for clf_name, (clf, defaults, search_space) in models.binary_class_models.items():
-    print("running classifier", clf_name)
+for X_train, X_test, y_train, y_test in load_datasets(challenge_nr=1):
+    results = []
+    losses = ('squared_hinge', 'hinge')
 
-    def make_fitness(clf_class, defaults):
-        def fitness(hyper_params):
-            hyper_params, = hyper_params
-            params = defaults
-            for space, param_value in zip(search_space, hyper_params):
-                if space['type'] == 'discrete':
-                    param_value = int(param_value)
-                if space['name'] == 'loss':
-                    param_value = losses[int(param_value)]
-                params[space['name']] = param_value
+    for clf_name, (clf, defaults, search_space) in models.binary_class_models.items():
+        print("running classifier", clf_name)
 
-            clf_ = clf_class(**params)
+        def make_fitness(clf_class, defaults):
+            def fitness(hyper_params):
+                # import ipdb; ipdb.set_trace()
+                hyper_params, = hyper_params
+                params = defaults
+                for space, param_value in zip(search_space, hyper_params):
+                    if space['type'] == 'discrete':
+                        param_value = int(param_value)
+                    if space['name'] == 'loss':
+                        param_value = losses[int(param_value)]
+                    params[space['name']] = param_value
 
-            t0 = time.time()
-            clf_.fit(X_train, y_train)
-            train_time = time.time() - t0
+                clf_ = clf_class(**params)
 
-            t0 = time.time()
-            pred = clf_.predict(X_test)
-            test_time = time.time() - t0
+                t0 = time.time()
+                clf_.fit(X_train, y_train)
+                train_time = time.time() - t0
 
-            score = sklearn.metrics.accuracy_score(y_test, pred)
-            return -score
-        return fitness
+                t0 = time.time()
+                pred = clf_.predict(X_test)
+                test_time = time.time() - t0
 
-    opt = GPyOpt.methods.BayesianOptimization(
-        f=make_fitness(clf, defaults),  # function to optimize
-        domain=search_space,  # box-constraints of the problem
-        acquisition_type='MPI',  # LCB acquisition
-        acquisition_weight=0.3,  # Exploration exploitation
-    )
-    design_space = GPyOpt.Design_space(search_space)
-    opt.run_optimization(max_iter=1)  # , verbosity=31337)
+                score = sklearn.metrics.accuracy_score(y_test, pred)
+                return -score
+            return fitness
 
-    # for k, v in zip(search_space, opt.x_opt):
-    #     print(k['name'], ":=", v, end=' ')
-    # print("scored:", opt.fx_opt)
-    results.append((clf_name, opt.fx_opt))
+        opt = GPyOpt.methods.BayesianOptimization(
+            f=make_fitness(clf, defaults),  # function to optimize
+            domain=search_space,  # box-constraints of the problem
+            acquisition_type='MPI',  # LCB acquisition
+            acquisition_weight=0.3,  # Exploration exploitation
+            initial_design_numdata=5,
+        )
+        design_space = GPyOpt.Design_space(search_space)
+        opt.run_optimization(max_iter=1)  # , verbosity=31337)
 
-print("=" * 20, "results", "=" * 20)
-print("\n" * 1)
-for k, v in sorted(results, key=lambda x: -x[1]):
-    print(k, v)
-print("=" * 20, "results", "=" * 20)
+        # for k, v in zip(search_space, opt.x_opt):
+        #     print(k['name'], ":=", v, end=' ')
+        # print("scored:", opt.fx_opt)
+        results.append((clf_name, opt.fx_opt))
+
+    print("=" * 20, "results", "=" * 20)
+    print("\n" * 1)
+    for k, v in sorted(results, key=lambda x: -x[1]):
+        print(k, v)
+    print("=" * 20, "results", "=" * 20)
